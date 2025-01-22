0e5964f8f3b2dfb851eb772de0a92d4bd09bca5f
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-22 21:39:26 +0000

    fixes made to tf variables and readme made clearer surrounding lambda function naming

--- variables.tf
+++ variables.tf
@@ -7,3 +7,18 @@ variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
 }
+
+variable "func_name" {
+  description = "name given to Lambda function that will appear in aws console"
+  type        = string
+}
+
+variable "iam_role_name" {
+  description = "Name given to iam policy that will appear in aws console"
+  type = string
+}
+
+variable "sqs_queue_name" {
+  description = "Name given to sqs queue that will appear in aws console"
+  type = string
+}
